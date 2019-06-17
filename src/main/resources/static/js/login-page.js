7bc6bbcf32de9618831aee7ca62b12a9f5116e44
Aaron Moran <aaronaaron@hotmail.co.uk>
2019-06-17 12:18:37 +0100

    progress on login and empty html for other pages

---
+++ src/main/resources/static/js/login-page.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	document.getElementById("loginForm").addEventListener("submit", loginFormSubmit, false);
+});
+
+function loginFormSubmit(e) {
+	e.preventDefault();
+	//TODO make ajax request verification
+}
+
+function navigateToHomePage() {
+	//On ajax success
+}
