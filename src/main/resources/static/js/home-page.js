cff06b81831df15b9f95cf93bab9eb58cdf749f3
Aaron Moran <aaronaaron@hotmail.co.uk>
2019-06-19 18:28:22 +0100

    added front end for delete quiz

--- src/main/resources/static/js/home-page.js
+++ src/main/resources/static/js/home-page.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
 	document.getElementById("addQuizButton").addEventListener("click", addQuiz, false);
+	setDeleteQuizHandler();
 });
+
+function setDeleteQuizHandler() {
+	var deleteQuizButtons = document.getElementsByClassName("deleteQuizButton");
+	for (var i = 0; i < deleteQuizButtons.length; i++) {
+		deleteQuizButtons[i].addEventListener("click", deleteQuiz, false);
+	}
+}
+
+function deleteQuiz(e) {
+	var quizEntry = e.target.parentElement;
+	var quizId = quizEntry.getAttribute("data-quiz-id");
+	var data = {"quizId": quizId};
+	var formData = JSON.stringify(data);
+	$.ajax({
+		type : "DELETE",
+		url : "/deleteQuiz",
+		data : formData,
+		contentType: "application/json; charset=utf-8",
+		success : function(result) {
+			if (result.status == "200") {
+				console.log("success");
+			} else {
+				console.log("ERROR:  ", "something went wrong");
+			}
+		},
+		error : function(e) {
+			console.log("ERROR: ", e);
+		}
+	});
+}
 
 function addQuiz(){
 	$.ajax({
@@ -9,6 +40,7 @@ function addQuiz(){
 		success : function(result) {
 			if (result.status == "200") {
 				console.log("success");
+				location.reload(true);
 			} else {
 				console.log("ERROR:  ", "something went wrong");
 			}
